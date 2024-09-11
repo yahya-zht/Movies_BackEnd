65ac1d6e000a9ea512b285e7f1401592e2861412
Yahya Dev <zhtyahya@gmail.com>
2024-09-11 14:16:23 +0200

    Enable CORS for all routes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const app = express();
+const cors = require("cors"); // Import cors middleware
 
+const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(express.json());
 
